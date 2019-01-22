7209c5629786a5d7bc780eaa28f955382b21d240
lenzottij <lenzotti.5@buckeyemail.osu.edu>
2019-01-22 18:28:18 -0500

    Basic styling for demo components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import DemoInput from './components/DemoInput'
 import DemoResults from './components/DemoResults'
-import Header from './components/Header'
+import DemoHeader from './components/DemoHeader'
 import Footer from './components/Footer'
 import './App.css'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <DemoHeader />
         <DemoInput />
         <DemoResults />
         <Footer />
