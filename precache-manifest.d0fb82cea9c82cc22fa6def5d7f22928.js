dd5478e5336c930d7e89a1dd160d41a13b75f6b5
lenzottij <lenzotti.5@buckeyemail.osu.edu>
2019-01-22 16:49:39 -0500

    Updates

---
+++ precache-manifest.d0fb82cea9c82cc22fa6def5d7f22928.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdb7d0f3ece98940e15c",
+    "url": "/CSE5914/static/js/runtime~main.bdb7d0f3.js"
+  },
+  {
+    "revision": "23628ea362c727207aaf",
+    "url": "/CSE5914/static/js/main.23628ea3.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/CSE5914/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "23628ea362c727207aaf",
+    "url": "/CSE5914/static/css/main.9d74be88.chunk.css"
+  },
+  {
+    "revision": "7595b468dad02e25109e5b52712dbf7c",
+    "url": "/CSE5914/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CSE5914/precache-manifest.b2e08429e4e3280c04f91ec2589142af.js"
+  "/CSE5914/precache-manifest.d0fb82cea9c82cc22fa6def5d7f22928.js"
 );
 
 workbox.clientsClaim();
