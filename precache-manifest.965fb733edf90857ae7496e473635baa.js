b94d425c9770b8df92486d770ed9ce5386968f27
lenzottij <lenzotti.5@buckeyemail.osu.edu>
2019-01-23 21:13:56 -0500

    Updates

---
+++ precache-manifest.965fb733edf90857ae7496e473635baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37d60c66ffcd28f51f97fa4e99274c65",
+    "url": "/CSE5914/static/media/logo.37d60c66.PNG"
+  },
+  {
+    "revision": "bdb7d0f3ece98940e15c",
+    "url": "/CSE5914/static/js/runtime~main.bdb7d0f3.js"
+  },
+  {
+    "revision": "d3f9a59f56100622482a",
+    "url": "/CSE5914/static/js/main.d3f9a59f.chunk.js"
+  },
+  {
+    "revision": "41cdf526697cda8002b4",
+    "url": "/CSE5914/static/js/1.41cdf526.chunk.js"
+  },
+  {
+    "revision": "d3f9a59f56100622482a",
+    "url": "/CSE5914/static/css/main.9581628f.chunk.css"
+  },
+  {
+    "revision": "5cac42bace8e97d6c18b0ca57955fe30",
+    "url": "/CSE5914/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/CSE5914/precache-manifest.d0fb82cea9c82cc22fa6def5d7f22928.js"
+  "/CSE5914/precache-manifest.965fb733edf90857ae7496e473635baa.js"
 );
 
 workbox.clientsClaim();
