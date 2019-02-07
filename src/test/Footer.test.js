3f7b0bee0421e73a679c0452306dae286783e4b2
lenzottij <lenzotti.5@buckeyemail.osu.edu>
2019-02-07 17:00:01 -0500

    Adding enzyme and some basic tests

---
+++ src/test/Footer.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Footer from '../components/Footer.js'
+
+describe('MyComponent', () => {
+  it('should render correctly', () => {
+    shallow(<Footer />);
+  });
+});
